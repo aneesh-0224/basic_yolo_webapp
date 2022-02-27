0a413924e4c6cc7c266690742d30493e1ff8e149
unknown <aneesh0224@gmail.com>
2022-02-27 12:11:48 +0530

    added basic error handling

--- yolo.py
+++ yolo.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, redirect, url_for,request
+from asyncio.windows_events import NULL
+from flask import Flask, render_template, flash 
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField,FileAllowed
 from wtforms import SubmitField
 import os
 
 app=Flask(__name__)
-#static_folder='WEB PAGE FOR YOLO/static'
 app.config['SECRET_KEY']='568325235'
 
 picFolder= os.path.join('static','pics')
@@ -26,17 +26,26 @@ def home_page():
     form = imgForm()
     print(form.validate_on_submit())
     if form.validate_on_submit():
-        img_file_name=save_img(form.p_img.data)
-        #Since after calling the save_img function the the image is stored in static/input it can be used
-        #as input for the model and the model can be instantiated here.and the output of the model can be 
-        #put in the static/pics folder.
-        tags=['tag1','tag2','tag3','tag4','tag5']
-        pics=[]
-        for i in range(4):
-            file_img = f'r{i+1}.jpeg'
-            print(file_img)
-            pics.append(os.path.join(app.config['UPLOAD_FOLDER'],file_img))
-        return render_template('show.html',path=app.root_path,tags=tags,user_imgs=pics)
+        try:
+            img_file_name=save_img(form.p_img.data)
+            print(form.p_img.type)
+            #Since after calling the save_img function the the image is stored in static/input it can be used
+            #as input for the model and the model can be instantiated here.and the output of the model can be 
+            #put in the static/pics folder.
+            tags=['tag1','tag2','tag3','tag4','tag5']
+
+            pics_temp=os.listdir('C:/Users/Aneesh Kulkarni/web_dev/flask projects/web page for yolo/static/pics')
+            pics=[]
+            for i in range(len(pics_temp)):
+                print(os.listdir('C:/Users/Aneesh Kulkarni/web_dev/flask projects/web page for yolo/static/pics'))
+                pics.append(os.path.join(app.config['UPLOAD_FOLDER'],pics_temp[i]))
+            return render_template('show.html',path=app.root_path,tags=tags,user_imgs=pics)
+        except:
+            flash('Please put in an image in valid format','error')
+            return render_template('homepage.html',form=form)
+    if(form.p_img.data is not None):
+        print(form.p_img.data)
+        flash('Please select an image only','error')
     return render_template('homepage.html',form=form)
 
 
